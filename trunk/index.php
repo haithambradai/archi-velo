68527a5a5181743b0b167d78c07ccd8f2ea0a866
quentincurtet@gmail.com <quentincurtet@gmail.com@ca9be38c-a1b0-f3c7-b63d-1ab8325fdc0a>
2015-01-30 14:08:59 +0000

    Ajout css bootstrap

--- trunk/index.php
+++ trunk/index.php
@@ -1,7 +1,13 @@
-
-<?php 
-echo '<h1> Ceci est un Test </h1>'
-
-
-
-?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Archi Velo</title>
+	<link rel="stylesheet" href="style.css" type="text/css"/>
+</head>
+<body>
+	<div class="container"><h1>Toto</h1></div>
+	<?php 
+		echo '<h1> Ceci est un Test </h1>'
+	?>
+</body>
+</html>
