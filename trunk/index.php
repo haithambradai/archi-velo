126ae5fcbf84f3e2c4142d0947e71937eb60815b
quentincurtet@gmail.com <quentincurtet@gmail.com@ca9be38c-a1b0-f3c7-b63d-1ab8325fdc0a>
2015-03-18 10:03:34 +0000

    Modif présentation et encodage

--- trunk/index.php
+++ trunk/index.php
@@ -1,14 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="fr">
 <head>
 	<title>Archi Velo</title>
 	<link rel="stylesheet" href="style.css" type="text/css"/>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<meta http-equiv="Content-Type" content="text/html;" charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-	<div class="container"><h1>Toto</h1></div>
-	<div class="span6">
-		<p><?php 
+	<div class="container">
+		<h1>Toto</h1>
+		<div class="row"><?php 
 			//Récupération du fichier JSON
 			$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
 			
@@ -22,34 +23,42 @@
 			$nbLigne = count($test);
 			?>
 			<!-- Tableau présentant les données -->
-			<table class="table table-bordered">
-				<tr><th>Nom</th><th>Adresse</th><th>Nombre de places libres</th><th>Nombre de vélos disponibles</th></tr>
-				<?php
-				//Boucle permettant de parcourir le tableau et de récupérer les bonnes valeurs du JSON
-				for($i=0;$i<$nbLigne;$i++){
-					//On récupère le nom
-					$name = $test[$i]['name'];
+			<div class="table-responsive">
+				<table class="table table-bordered">
+					<tr><th class="col-md-4">Nom</th><th class="col-md-4">Nombre de places libres</th><th class="col-md-4">Nombre de vélos disponibles</th></tr>
+					<?php
+						//Boucle permettant de parcourir le tableau et de récupérer les bonnes valeurs du JSON
+						for($i=0;$i<$nbLigne;$i++){
+							//On récupère le nom
+							$name = $test[$i]['name'];
 
-					//On récupère l'adresse
-					$address = $test[$i]['address'];
+							//On récupère l'adresse
+							$address = $test[$i]['address'];
 
-					//On récupère juste le nom de la station pas son identifiant donc on extrait la partie que l'on veut
-					list($num, $nom) = explode('-', $name);
+							//On récupère juste le nom de la station pas son identifiant donc on extrait la partie que l'on veut
+							list($num, $nom) = explode('-', $name);
 
-					//Récupération des coordonnées de la station
-					$lat = $test[$i]['position']['lat'];
-					$lng = $test[$i]['position']['lng'];
+							//Récupération des coordonnées de la station
+							$lat = $test[$i]['position']['lat'];
+							$lng = $test[$i]['position']['lng'];
 
-					//Récupération des infos de la station : nb de vélos et le nb de places dispo
-					$nbVelos = $test[$i]['available_bikes'];
-					$nbPlaceLibre = $test[$i]['available_bike_stands'];
+							//Récupération des infos de la station : nb de vélos et le nb de places dispo
+							$nbVelos = $test[$i]['available_bikes'];
+							$nbPlaceLibre = $test[$i]['available_bike_stands'];
 
-					//Affichage des données
-					echo '<tr><td>'.$nom.'</td><td>'.$address.'</td><td>'.$nbVelos.'</td><td>'.$nbPlaceLibre.'</td></tr>';
-				}
-			?>
-			</table>
-		</p>
+							$nom_encode = str_replace("°", "ème", $nom);
+							$adresse_encode = str_replace("n°", "Numéro ", $address);
+							$adresse_good = str_replace("°", "ème", $adresse_encode);
+
+
+
+							//Affichage des données
+							echo '<tr><td class="col-md-4"><b>'.ucwords($nom_encode).' :</b><br>'.ucwords($adresse_good).'</td><td class="col-md-4">'.$nbVelos.'</td><td class="col-md-4">'.$nbPlaceLibre.'</td></tr>';
+						}
+					?>
+				</table>
+			</div>
+		</div>
 	</div>
 </body>
 </html>
