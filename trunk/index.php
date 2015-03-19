7235738b5719cc2f4f58a79c13e2de60061018f8
quentincurtet@gmail.com <quentincurtet@gmail.com@ca9be38c-a1b0-f3c7-b63d-1ab8325fdc0a>
2015-03-19 13:35:48 +0000

    Modif en tentant la map

--- trunk/index.php
+++ trunk/index.php
@@ -4,11 +4,15 @@
 	<title>Archi Velo</title>
 	<link rel="stylesheet" href="style.css" type="text/css"/>
 	<meta http-equiv="Content-Type" content="text/html;" charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+	<style type="text/css">#map_canvas { width: 100%; height: 100% }</style>
 </head>
 <body>
 	<div class="container">
-		<h1>Toto</h1>
+		<h1>Archi-Vélo</h1>
+		<button class="btn btn-default" onclick="initialize()">Localiser Moi </button>
+		
 		<div class="row"><?php 
 			//Récupération du fichier JSON
 			$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
@@ -25,7 +29,7 @@
 			<!-- Tableau présentant les données -->
 			<div class="table-responsive">
 				<table class="table table-bordered">
-					<tr><th class="col-md-4">Nom</th><th class="col-md-4">Nombre de places libres</th><th class="col-md-4">Nombre de vélos disponibles</th></tr>
+					<tr><th class="col-md-3">Nom</th><th class="col-md-3">Map Google</th><th class="col-md-3">Nombre de places libres</th><th class="col-md-3">Nombre de vélos disponibles</th></tr>
 					<?php
 						//Boucle permettant de parcourir le tableau et de récupérer les bonnes valeurs du JSON
 						for($i=0;$i<$nbLigne;$i++){
@@ -53,12 +57,74 @@
 
 
 							//Affichage des données
-							echo '<tr><td class="col-md-4"><b>'.ucwords($nom_encode).' :</b><br>'.ucwords($adresse_good).'</td><td class="col-md-4">'.$nbVelos.'</td><td class="col-md-4">'.$nbPlaceLibre.'</td></tr>';
+							echo '<tr><td class="col-md-3"><b>'.ucwords($nom_encode).' :</b><br>'.ucwords($adresse_good).'</td><td class="col-md-3"><div id="map_canvas"></div></td><td class="col-md-3">'.$nbVelos.'</td><td class="col-md-3">'.$nbPlaceLibre.'</td></tr>';
 						}
 					?>
 				</table>
 			</div>
 		</div>
 	</div>
+	<div id="map_canvas"></div>
+	<form id="form" action=index.php method=POST>
+	  <input type=hidden id="Lat" name="Lat" value="">
+	  <input type=hidden id="Long" name="Long" value="">
+	</form>
+	<script type="text/javascript">
+	  	var previousPosition = null;
+				
+		function initialize()
+		{
+			var x;
+			var y;
+			 if (navigator.geolocation) {
+		        navigator.geolocation.getCurrentPosition(showPosition);
+
+		    } else { 
+		        x.innerHTML = "Geolocation is not supported by this browser.";
+		    }
+		}
+
+		function showPosition(loc)
+		{
+			//alert(loc.coords.latitude);
+			map = new google.maps.Map(document.getElementById("map_canvas"), {
+				zoom: 19,
+				center: new google.maps.LatLng(loc.coords.lattitude,loc.coords.longitude),
+				mapTypeId: google.maps.MapTypeId.ROADMAP
+			});   
+		 
+			if (navigator.geolocation){
+			  var watchId = navigator.geolocation.watchPosition(successCallback,
+			                            null,
+			                            {enableHighAccuracy:true});
+			}else{
+				alert("Votre navigateur ne prend pas en compte la géolocalisation HTML5");    
+			}
+		}
+
+		function successCallback(position){
+		  map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
+		  var marker = new google.maps.Marker({
+		    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude), 
+		    map: map
+		  }); 
+		  if (previousPosition){
+		    var newLineCoordinates =
+		    [
+		      new google.maps.LatLng(previousPosition.coords.latitude, previousPosition.coords.longitude),
+		      new google.maps.LatLng(position.coords.latitude, position.coords.longitude)
+		    ];
+		         
+		    var newLine = new google.maps.Polyline({
+		      path: newLineCoordinates,        
+		      strokeColor: "#FF0000",
+		      strokeOpacity: 1.0,
+		      strokeWeight: 2
+		    });
+		    newLine.setMap(map);
+		  }
+		  previousPosition = position;
+		}
+	</script>
 </body>
 </html>
