a88c80f112076cd48e2fbb6e62dbea4174b7a15a
quentincurtet <quentincurtet@gmail.com>
2015-04-02 11:18:55 +0000

    C'est tout bon c'est fini !

--- index.php
+++ index.php
@@ -12,6 +12,16 @@
 </head>
 <body>
 	<h1 style="text-align:center; font-size:72px">Archi-Vélo</h1>
+	<div class="container" style="margin-bottom:70px; text-align:center">
+		<h2 style="margin-bottom:35px">Dans quelle ville êtes vous ?</h2>
+		<a href="index.php?ville=Lyon"><button type="button" class="btn btn-primary" style="margin-right:35px; width: 150px;">Lyon</button></a>
+	    <a href="index.php?ville=Paris"><button type="button" class="btn btn-danger" style="margin-right:35px; width: 150px;">Paris</button></a>
+	    <a href="index.php?ville=Marseille"><button type="button" class="btn btn-info" style="margin-right:35px; width: 150px;">Marseille</button></a>
+	    <a href="index.php?ville=Toulouse"><button type="button" class="btn btn-warning" style="width: 150px;">Toulouse</button></a>		
+	</div>
+	<div class="container">
+		<div><h4 style="float:left">Légende : </h4><p style="padding-top: 7px; float:left; margin-right:10px"><img src="images/yes_velo.png">Vélos et places disponibles</p><p style="padding-top: 7px; float:left; margin-right:10px"><img src="images/no_velo.png">Plus de vélos disponibles</p><p style="padding-top: 7px; float:left;  margin-right:10px"><img src="images/place_indispo.png">Plus de places disponibles</p><p style="padding-top: 7px; float:left; margin-right:10px"><img src="images/warn_velo.png">Peu de vélos disponibles</p><p style="padding-top: 7px; float:left;"><img src="images/warn_place.png">Peu de places disponibles</p></div>
+	</div>
 	<div id="map_canvas"></div>
 	<script type="text/javascript">
 		map = new google.maps.Map(document.getElementById("map_canvas"), {  
@@ -24,6 +34,9 @@
 		  map.panTo(new google.maps.LatLng(position.coords.latitude, position.coords.longitude));
 		  var contentMoi = '<div id="content"><div id="siteNotice"><div><h1 id="firstHeading" class="firstHeading" style="font-size:24px">Vous êtes ici !</h1></div>';
 		  var infowindow = new google.maps.InfoWindow({content: contentMoi});
+		  if (marker) {
+		  		marker = google.maps.Marker({position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude)});
+		  }
 		  var marker = new google.maps.Marker({
 		    position: new google.maps.LatLng(position.coords.latitude, position.coords.longitude), 
 		    map: map
@@ -33,8 +46,18 @@
 	</script>
 	<div class="container">
 		<div class="row"><?php 
-			//Récupération du fichier JSON
-			$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
+			if (isset($_GET['ville']) && !empty($_GET['ville']) && $_GET['ville'] == 'Lyon') {
+				$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Lyon&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c');
+			}
+			else if (isset($_GET['ville']) && !empty($_GET['ville']) && $_GET['ville'] == 'Paris'){
+				$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Paris&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
+			}
+			else if (isset($_GET['ville']) && !empty($_GET['ville']) && $_GET['ville'] == 'Marseille'){
+				$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Marseille&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
+			}
+			else if (isset($_GET['ville']) && !empty($_GET['ville']) && $_GET['ville'] == 'Toulouse'){
+				$json = file_get_contents('https://api.jcdecaux.com/vls/v1/stations?contract=Toulouse&apiKey=05011dd5abfb13c9db88e0f585644d4cac5a416c'); 
+			}
 			
 			//Le var_dump est à décommenter si on veut récupérer d'autres infos dans le JSON pour voir le nom du bon champ
 			//var_dump(json_decode($json));
@@ -77,8 +100,23 @@
 							//echo '<tr><td class="col-md-3"><b>'.ucwords($nom_encode).'</b></td><td class="col-md-3">'.$lat.'<br/>'.$lng.'</td><td class="col-md-3">'.$nbVelos.'</td><td class="col-md-3">'.$nbPlaceLibre.'</td></tr>';
 							
 							echo '<script type="text/javascript">
+									if (('.$nbVelos.' > 2) && ('.$nbPlaceLibre.' > 2)) {
+										var image = "images/yes_velo.png"
+									}
+									else if ('.$nbVelos.' == 0){
+										var image = "images/no_velo.png"
+									}
+									else if ('.$nbPlaceLibre.' == 0) {
+										var image = "images/place_indispo.png"
+									}
+									else  if (('.$nbVelos.' == 1) || ('.$nbVelos.' == 2)){
+										var image = "images/warn_velo.png"
+									}
+									else  if (('.$nbPlaceLibre.' == 1) || ('.$nbPlaceLibre.' == 2)){
+										var image = "images/warn_place.png"
+									}
 									var contentString'.$i.' = \'<div id="content"><div id="siteNotice"><div><h1 id="firstHeading" class="firstHeading" style="font-size:24px">'.addslashes($nom_encode).'</h1><div id="bodyContent"><p>Nombre de place disponibles : '.$nbPlaceLibre.'</p><p>Nombre de vélos disponibles : '.$nbVelos.'</p></div></div>\';
-									var myMarker'.$i.' = new google.maps.Marker({position: {lat: '.$lat.', lng: '.$lng.'},title:"'.$nom_encode.'", icon: "http://www.geovelo.fr/static/img/pictos/velov.png", map:map});
+									var myMarker'.$i.' = new google.maps.Marker({position: {lat: '.$lat.', lng: '.$lng.'},title:"'.$nom_encode.'", icon: image, map:map});
 									myMarker'.$i.'.setMap(map); 
 									google.maps.event.addListener( myMarker'.$i.', \'click\', function() {
 										var infowindow'.$i.' = new google.maps.InfoWindow({
