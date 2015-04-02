8293e37982d4f6afd2157c67f18565207ad2aa9c
quentincurtet@gmail.com <quentincurtet@gmail.com@ca9be38c-a1b0-f3c7-b63d-1ab8325fdc0a>
2015-04-02 07:48:34 +0000

    Tout marche on passe aux villes

--- trunk/index.php
+++ trunk/index.php
@@ -15,7 +15,7 @@
 	<div id="map_canvas"></div>
 	<script type="text/javascript">
 		map = new google.maps.Map(document.getElementById("map_canvas"), {  
-		        zoom: 17,
+		        zoom: 16,
 		        center: new google.maps.LatLng(48.858565, 2.347198),
 		        mapTypeId: google.maps.MapTypeId.ROADMAP
 		      });   
